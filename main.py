9eb91ea07988795ce908d7630486863277a840ae
test_python <khazaei.amir110@gmail.com>
2023-10-18 14:22:17 +0330

    current_process

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from multiprocessing import Process
+from multiprocessing import Process , current_process
 import time
 
 start = time.perf_counter()
 def show(name):
     print(f'Starting {name} ...')
-    time.sleep(4)
+    time.sleep(1)
+    print(current_process())
     print(f'Ending {name} ...')
 
-p1 = Process(target=show, args=('One',))
-p2 = Process(target=show, args=('Two',))
+p1 = Process(target=show,name='process_1', args=('One',))
+p2 = Process(target=show,name='process_2', args=('Two',))
 
 end = time.perf_counter()
 
