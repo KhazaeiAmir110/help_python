53a29afe258029b609ecbd1eb769d9f6ef18bb87
test_python <khazaei.amir110@gmail.com>
2023-10-26 13:56:40 +0330

    Chess

---
+++ شطرنج حرفه ای.py
@@ -0,0 +1,3 @@
+#king, queen, rook, bishop, knight, pawn 
+shah , vazir , rokh , phil , asbe , sarbaz = map (int , input ().split())
+print(f"{1-shah} {1-vazir} {2-rokh} {2-phil} {2-asbe} {8-sarbaz}")
