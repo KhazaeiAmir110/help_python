c85d666270e080f27009743cd8e64dc8c8396d29
test_python <khazaei.amir110@gmail.com>
2023-10-26 13:56:08 +0330

    The completeness of the number

---
+++ کامل بودن یا نبودن.py
@@ -0,0 +1,13 @@
+# input (*)
+#
+a = int(input())
+su = 0
+for i in range (1 , a):
+      if  a % i ==0 :
+            su += i
+
+if su == a  :
+      print ('YES')
+else :
+      print ('NO')
+      
