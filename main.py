84bd15d721de52b4aadb20599e097cf8efd4e89b
test_python <khazaei.amir110@gmail.com>
2023-11-10 14:08:41 +0330

    dtype array

--- main.py
+++ main.py
@@ -13,3 +13,12 @@ print(array)
 print(array.ndim)
 # ستون و ردیف
 print(array.shape)
+
+# تبدیل نوع داده به float
+# باید یک نوع داه باشد تمام داده های ما
+array_2 = np.array([
+    [2, 3.4]
+], dtype=np.uint8)
+print(array_2)
+#uint8
+print(array_2.dtype)
