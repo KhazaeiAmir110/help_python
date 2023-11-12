00794812d38bff995097213757a96161b0765009
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-11-12 17:00:20 +0330

    size , reshape, index[i]

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import numpy as np
 
-array = np.array([
-    [1,2],
-    [3,4],
-    [5,6]
-])
-# type array
-print(type(array))
-#print
-print(array)
-# بعد
-print(array.ndim)
-# ستون و ردیف
-print(array.shape)
+"""
+کار با متد های بیشتر numpy
+عملیات هایی که میتوانیم روی آرایه ها انجام دهیم
+part 2
+"""
 
-# تبدیل نوع داده به float
-# باید یک نوع داه باشد تمام داده های ما
-array_2 = np.array([
-    [2, 3.4]
-], dtype=np.uint8)
-print(array_2)
-#uint8
-print(array_2.dtype)
+array = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12],
+    [100, 200, 300]
+])
+
+print(array)
+
+# size array
+print('siz = ' + str(array.size))
+
+print('n . m = ' + str(array.shape))
+# عوض کردن ساختار آرایه
+print('-----------------')
+print(array.reshape(5, 3))
+print(array.reshape(1, 15))
+print(array.reshape(15, 1))
+
+# item [n]
+print('itme[0] = ' + str(array[0]))
+# one
+print('itme[0][0] = ' + str(array[0][0]))
+# two
+print('itme[0, 0] = ' + str(array[0, 0]))
+# range
+print('itme[0:3] = ' + str(array[0:3]))
