7d2449f68dbe22d62ce6ec2d42a27127a73dacb7
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-11-13 18:35:07 +0330

    Mathematical operations

--- main.py
+++ main.py
@@ -1,40 +1,23 @@
 import numpy as np
 
 """
-متد های دیگر numpy به غیر از array
+عملیات های ریاضی روی آرایه ها
 """
 
-# empty
-array = np.empty((3, 4), dtype=np.uint8)
-print(array)
+array_1 = np.array([
+    [0, 1, 2]
+])
+array_2 = np.array([
+    [3, 4, 5]
+])
 
-# empty_like
-test_array = ([1, 2, 3], [4, 5, 6])
-array_1 = np.empty_like(test_array, shape=(3, 4), dtype=np.uint8)
-print(array_1)
+print(array_1 + array_2)
 
-# eye => matrix I : n * m
-array_2 = np.eye(4, 5)
-array_3 = np.eye(4, 5, 1)
-print(array_2)
-print(array_3)
+print(array_1 + 1)
 
-# identity => matrix I : n * n
-array_4 = np.identity(5)
-print(array_4)
+print(array_1 == array_2)
 
-# ones => matrix 1
-array_5 = np.ones((2, 3))
-print(array_5)
+print(np.sum(array_1))
 
-# zeros => matrix 0
-array_6 = np.zeros((2, 3))
-print(array_6)
-
-# full => matrix n
-array_n = np.full((3, 3), 10)
-print(array_n)
-
-# arange => m => n
-array_arang = np.arange(10, 200)
-print(array_arang)
+print(array_1.min())
+print(array_1.max())
