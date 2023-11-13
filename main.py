69b2bf625ce6ec8e68050eb0b212863544d708fd
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-11-13 18:39:16 +0330

    Broadcasting

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import numpy as np
 
 """
-عملیات های ریاضی روی آرایه ها
+Broadcasting
 """
 
 array_1 = np.array([
-    [0, 1, 2]
+    [1, 2, 3]
 ])
 array_2 = np.array([
-    [3, 4, 5]
+    [1, 2, 3],
+    [4, 5, 6]
 ])
 
 print(array_1 + array_2)
-
-print(array_1 + 1)
-
-print(array_1 == array_2)
-
-print(np.sum(array_1))
-
-print(array_1.min())
-print(array_1.max())
