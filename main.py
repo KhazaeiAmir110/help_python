48341cd9b705cfb6fc23f228fe88e0ec46c356c8
test_python <khazaei.amir110@gmail.com>
2023-11-16 13:29:40 +0330

    Structured arrays https://numpy.org/doc/stable/user/basics.rec.html

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
 import numpy as np
 
 """
-Universal functions  => ufunc
-
-vectorized : زمانی اتفاق می افتد که این کتاب خانه کد برنامه را به زبان پایتون اجرا نمیکند 
-بلکه به زبان سی اجرا میکند به دلیل سرعت بیشتر برنامه
-
-در آرایه پایتون هر نوع داده ای میتوان نوشت بلی در آرایه این کتابخانه فقط یک نوع داده در آرایه وجود دارد تا سرعت را بالا ببرد
+Structured arrays
+در بعضی مواقع نیاز است که ما در آرایه خود ، بیش از یک نوع داده را استفاده کنیم ، str , int , float , ... 
 """
 
-# طبق آرایه اول ، آرایه دوم را n برابر میکند
-array_1 = np.multiply.outer([1,2,3,4,5], [4,5,6])
-print(array_1)
+array_human = np.array(
+    [('amir', 20, 30.9, 'qom'), ('reza', 40, 80.9, 'tehran'), ('ali', 25, 50.9, 'mashhad')],
+    dtype = [('name','<U10'), ('age','<i4'), ('weight', '<f4'), ('city', '<U10')]
+)
+print(array_human)
 
-# ضرب اعداد داخل آرایه و خروجی جواب فقط آرایه تک بعدی
-array_2 = np.multiply.reduce([1,2,3,4,5])
-print(array_2)
+print(array_human[0]['name'])
 
-# مشخص کردن n * m بودن آرایه
-array_3 = np.arange(8).reshape((2,4))
-array_4 = np.arange(8).reshape((2,2,2))
-print(array_3)
-print(array_4)
-
-# جمع عناصر داخل یک آرایه
-array_5 = np.add.reduce(np.arange(-10,11))
-print(array_5)
+for record in array_human:
+    if record['age'] >= 25 :
+        print(record)
