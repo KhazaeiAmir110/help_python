6f8db282925bbc0d12609c1ba74a178859370cbb
test_python <khazaei.amir110@gmail.com>
2023-11-16 14:13:20 +0330

    Sorting, searching, and counting https://numpy.org/doc/stable/reference/routines.sort.html#sorting

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import numpy as np
 
 """
-Structured arrays
-در بعضی مواقع نیاز است که ما در آرایه خود ، بیش از یک نوع داده را استفاده کنیم ، str , int , float , ... 
+Sorting, searching, and counting
+
 """
 
-array_human = np.array(
-    [('amir', 20, 30.9, 'qom'), ('reza', 40, 80.9, 'tehran'), ('ali', 25, 50.9, 'mashhad')],
-    dtype = [('name','<U10'), ('age','<i4'), ('weight', '<f4'), ('city', '<U10')]
-)
-print(array_human)
+array_number_1 = np.array([8, 7, 6, 5, 43, 2, 23, 5, 6, 6, 4, 3, 0,0,0])
 
-print(array_human[0]['name'])
+array_number_2 = np.array([
+    [43, 4, 34, 3, 43],
+    [100, 12, 1, 2, 33]
+])
 
-for record in array_human:
-    if record['age'] >= 25 :
-        print(record)
+array_name = ('amir', 'reza', 'ali', 'dfd', 'amir')
+
+# Sort
+# method 1 => new array
+array_new = np.sort(array_number_1)
+print(array_new)
+# method 2 => sort in array
+array_number_2.sort()
+print(array_number_2)
+# method 3 => sort by index => output = index array
+print(np.argsort(array_number_1))
+
+# Searching
+# max , min => output index
+print(np.argmax(array_new))
+print(np.argmin(array_number_1))
+# Condition
+a = np.extract(np.mod(array_number_1, 2)==0, array_number_1)
+print(a)
+
+# cont => شماش اعداد غیر 0
+b = np.count_nonzero(array_number_1)
+print(b)
