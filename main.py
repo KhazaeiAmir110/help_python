fae2b460477afe08e9a73e4c925351c06abbd654
test_python <khazaei.amir110@gmail.com>
2023-11-16 14:27:57 +0330

    random https://numpy.org/doc/stable/reference/random/generator.html

--- main.py
+++ main.py
@@ -1,37 +1,18 @@
 import numpy as np
 
 """
-Sorting, searching, and counting
-
+random
 """
+random = np.random.default_rng()
+# int
+test_1 = random.integers(1,10,3)
+print(test_1)
 
-array_number_1 = np.array([8, 7, 6, 5, 43, 2, 23, 5, 6, 6, 4, 3, 0,0,0])
+# float
+test_2 = random.random(3)
+print(test_2)
 
-array_number_2 = np.array([
-    [43, 4, 34, 3, 43],
-    [100, 12, 1, 2, 33]
-])
-
-array_name = ('amir', 'reza', 'ali', 'dfd', 'amir')
-
-# Sort
-# method 1 => new array
-array_new = np.sort(array_number_1)
-print(array_new)
-# method 2 => sort in array
-array_number_2.sort()
-print(array_number_2)
-# method 3 => sort by index => output = index array
-print(np.argsort(array_number_1))
-
-# Searching
-# max , min => output index
-print(np.argmax(array_new))
-print(np.argmin(array_number_1))
-# Condition
-a = np.extract(np.mod(array_number_1, 2)==0, array_number_1)
-print(a)
-
-# cont => شماش اعداد غیر 0
-b = np.count_nonzero(array_number_1)
-print(b)
+# array
+array = np.array([2,3,4,5,6,7,8])
+test_3 = random.choice(array,2,p=[0.3,0,0,0.4,0,0,0.3])
+print(test_3)
