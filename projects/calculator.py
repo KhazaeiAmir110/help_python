0e4c42d6fb8d237e11b29ae26235604c792986ca
test_python <khazaei.amir110@gmail.com>
2023-11-29 18:17:33 +0330

    start miniproject calculator

---
+++ projects/calculator.py
@@ -0,0 +1,14 @@
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout 
+
+
+class MyGridLayout(GridLayout):
+	pass
+
+class CalculatorApp(App):
+	def build(self):
+		pass
+
+if __name__ == '__main__':
+	CalculatorApp().run()
