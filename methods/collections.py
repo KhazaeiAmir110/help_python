97dba1f210f43e6ff61f4b6a45638e6272e15c32
test_python <khazaei.amir110@gmail.com>
2023-12-17 20:56:25 +0330

    method ChainMap

--- methods/collections.py
+++ methods/collections.py
@@ -14,3 +14,28 @@ print(queue.index(3))
 print(queue.count(4))
 
 print(queue)
+
+# ===============================================
+
+from collections import ChainMap
+""" 
+    In contrast, writes, updates, and deletions only operate on the first
+     mapping.
+"""
+
+c1 = {
+    'amir': 12,
+    'ali': 13,
+    'ahmad': 14,
+    'alice': 15
+}
+
+c2 = {
+    'reza': 20,
+    'ramin': 21
+}
+
+c3 = {'hossein': 22}
+
+
+print(ChainMap(c1, c2, c3))
