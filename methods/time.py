e0120d83f8a3d0fae5f2e167e9b078ca4fb205b4
test_python <khazaei.amir110@gmail.com>
2023-12-17 14:18:46 +0330

    method datetime

--- methods/time.py
+++ methods/time.py
@@ -1,45 +1,67 @@
-import time
+# import time
+#
+#
+# # gmtime()
+# print(time.gmtime())
+#
+# # time() => epoch : 1-1-1970 00:00:00
+# print(time.time())
+#
+# # ctime
+# print(time.ctime())
+#
+#
+# # sleep
+# """
+# print(time.sleep(5))
+# print("Hello word")
+# """
+#
+# # srtftime()
+# """
+#     %Y  Year with century as a decimal number.
+#     %m  Month as a decimal number [01,12].
+#     %d  Day of the month as a decimal number [01,31].
+#     %H  Hour (24-hour clock) as a decimal number [00,23].
+#     %M  Minute as a decimal number [00,59].
+#     %S  Second as a decimal number [00,61].
+#     %z  Time zone offset from UTC.
+#     %a  Locale's abbreviated weekday name.
+#     %A  Locale's full weekday name.
+#     %b  Locale's abbreviated month name.
+#     %B  Locale's full month name.
+#     %c  Locale's appropriate date and time representation.
+#     %I  Hour (12-hour clock) as a decimal number [01,12].
+#     %p  Locale's equivalent of either AM or PM.
+# """
+# print(time.strftime("%a"))
+#
+# # Calculation time
+#
+# # perf_counter => user
+# print(time.perf_counter())
+#
+# # process_time => cpu
+# print(time.process_time())
 
 
-# gmtime()
-print(time.gmtime())
+import  datetime
 
-# time() => epoch : 1-1-1970 00:00:00
-print(time.time())
-
-# ctime
-print(time.ctime())
-
-
-# sleep
+time_1 = datetime.time(5,4,3)
+print(time_1)
 """
-print(time.sleep(5))
-print("Hello word")
+Arguments:
+
+        hour, minute (required)
+        second, microsecond (default to zero)
+        tzinfo (default to None)
+        fold (keyword only, default to zero)
+        """
+
+print(datetime.date(2024, 5,3))
+"""
+Arguments:
+
+        year, month, day (required, base 1)
 """
 
-# srtftime()
-"""
-    %Y  Year with century as a decimal number.
-    %m  Month as a decimal number [01,12].
-    %d  Day of the month as a decimal number [01,31].
-    %H  Hour (24-hour clock) as a decimal number [00,23].
-    %M  Minute as a decimal number [00,59].
-    %S  Second as a decimal number [00,61].
-    %z  Time zone offset from UTC.
-    %a  Locale's abbreviated weekday name.
-    %A  Locale's full weekday name.
-    %b  Locale's abbreviated month name.
-    %B  Locale's full month name.
-    %c  Locale's appropriate date and time representation.
-    %I  Hour (12-hour clock) as a decimal number [01,12].
-    %p  Locale's equivalent of either AM or PM.
-"""
-print(time.strftime("%a"))
-
-# Calculation time
-
-# perf_counter => user
-print(time.perf_counter())
-
-# process_time => cpu
-print(time.process_time())
