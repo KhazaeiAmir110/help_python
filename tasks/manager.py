62efc1ae98fa63edf658e3b05b8dea9ba1fb52ce
test_python <khazaei.amir110@gmail.com>
2024-01-13 07:46:39 +0330

    add method delete , filter, get

--- tasks/manager.py
+++ tasks/manager.py
@@ -11,6 +11,39 @@ class UserManager:
     def append(self, user: 'User'):
         self.users.append(user)
 
+    def delete(self, *args, **kwargs):
+        for _user in self.users:
+            print(vars(_user))
+            for key, value in kwargs.items():
+                if getattr(_user, key) == value:
+                    self.users.remove(_user)
+                    # return vars(_user)
+
+    def filter(self,*args, **kwargs):
+        _result = list()
+        for _user in self.users:
+            for key, value in kwargs.items():
+                if getattr(_user, key) == value:
+                    _result.append(_user)
+
+        return _result
+
+    def get(self, **kwargs):
+        get_list = list()
+        for _user in self.users:
+            for key, value in kwargs.items():
+                if getattr(_user, key) == value:
+                    get_list.append(_user)
+        if len(get_list) == 0:
+            return None
+        elif len(get_list) > 1:
+            return ValueError("ERROR : User More than one")
+        else:
+            return vars(get_list[0])
+
+    def list(self):
+        for _ in self.users:
+            print(vars(_))
 
 class User:
     objects = UserManager()
