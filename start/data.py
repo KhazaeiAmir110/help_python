09b8863bf687cc6087a33d758c9e5814552a69cc
test_python <khazaei.amir110@gmail.com>
2024-05-09 08:46:27 +0330

    add data

---
+++ start/data.py
@@ -0,0 +1,34 @@
+class Person:
+    def __init__(self, *args, **kwargs):
+        self.name = kwargs['name']
+        self.family = kwargs['family']
+        self.age = kwargs['age']
+        self.email = kwargs['email']
+        self.phone = kwargs['phone']
+        self.address = kwargs['address']
+        self.birthday = kwargs['birthday']
+        self.gender = kwargs['gender']
+
+        def getter(self, *args):
+            if hasattr(self,args):
+                return getattr(self,args)
+            else:
+                raise AttributeError(f'Argument {args} does not exist!!!')
+
+person_1 = Person(name="amir", family='df', age=20, email="<EMAIL>", phone="02442424242", address="sdfsdfsdf", birthday="dfd", gender="female")
+person_2 = Person(name="alice", family='df', age=20, email="<EMAIL>", phone="02442424242", address="sdfsdfsdf", birthday="sdfd", gender="female")
+
+content = {
+    'person_1': person_1,
+    'person_2': person_2,
+    'name': 'Amir',
+    'age': 20,
+    'list': [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    'dict': [
+        {'name': 'John', 'age': 12},
+        {'name': 'Aer', 'age': 23},
+        {'name': 'Jan', 'age': 19},
+        {'name': 'are', 'age': 40},
+        {'name': 'John', 'age': 42},
+    ]
+}
--- start/start.py
+++ start/start.py
@@ -1,4 +1,5 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import data
 
 env = Environment(
     loader=FileSystemLoader('./templates'),
@@ -7,17 +8,7 @@ env = Environment(
 
 template = env.get_template('index.html')
 
-content = {
-    'name':'Amir',
-    'age':20,
-    'list':[1,2,3,4,5,6,7,8,9],
-    'dict':[
-        {'name':'John', 'age':12},
-        {'name':'Aer', 'age':23},
-        {'name':'Jan', 'age':19},
-        {'name':'are', 'age':40},
-        {'name':'John', 'age':42},
-    ]
-}
+
+content = data.content
 
 print(template.render(**content))
