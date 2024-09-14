f2798bed48319decfb3b0bdc95e1e1d9bb68de07
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-09-14 16:13:04 +0330

    Encapsulation

--- base/oop_1.py
+++ base/oop_1.py
@@ -19,9 +19,28 @@ class Concrete(Base):
     def transfer(self):
         print('transfer')
 
-# a = Base()
-
 
 # Encapsulation
 
+class Student:
+    def __init__(self):
+        self.__age = 0
 
+    @property
+    def age(self) -> int:
+        return self.__age
+
+    @age.setter
+    def age(self, new_age):
+        self.__age = new_age
+
+    @age.deleter
+    def age(self):
+        del self.__age
+
+
+student = Student()
+
+student.age = 22
+print("age student : " + str(student.age))
+del student.age
