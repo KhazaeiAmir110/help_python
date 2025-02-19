91f432da35f2b19ee012a1761f4a49039c22e1fb
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2025-02-19 10:34:39 +0330

    apply_async

--- start/first.py
+++ start/first.py
@@ -1,3 +1,14 @@
+import time
+
 from celery import Celery
 
-app = Celery('tasks', broker='redis://localhost:6379/0')
+app = Celery(main='tasks', broker='amqp://guest:guest@localhost:5672/')
+
+
+@app.task(name='first.adding')
+def add(a, b):
+    time.sleep(20)
+    return a + b
+
+# add.delay(1,2)
+# add.apply_async(args=[1, 2], queue='first.adding', countdown=10, expires=30)
