5ca36c4273f078c77566abac013cf47e12dff827
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2025-02-19 12:26:27 +0330

    config celery

--- start/keeping_result.py
+++ start/keeping_result.py
@@ -2,6 +2,8 @@ from celery import Celery
 
 app = Celery('keep-result', broker='amqp://guest:guest@localhost:5672/', backend='rpc://')
 
+app.config_from_object('celery_config')
+
 
 @app.task(name="keep")
 def full_name(name, family):
