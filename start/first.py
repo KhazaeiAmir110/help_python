877373e3bddd1345552d8f420542b274e37fbb2c
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2025-02-19 10:56:26 +0330

    bind

--- start/first.py
+++ start/first.py
@@ -10,5 +10,15 @@ def add(a, b):
     time.sleep(20)
     return a + b
 
+
 # add.delay(1,2)
 # add.apply_async(args=[1, 2], queue='first.adding', countdown=10, expires=30)
+
+
+@app.task(bind=True, name='first.division')
+def division(self, a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print("Sorry, division by zero")
+        self.retry()
