72ad076cf1e6a3eb907bbded915d546ce634d6ba
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2025-03-01 11:15:23 +0330

    update session 4

--- learning/session4.py
+++ learning/session4.py
@@ -1,12 +1,12 @@
 import cv2 as cv
 
-img1 = cv.imread("../images/2.jpg")
-img2 = cv.imread("../images/3.jpg")
+img1 = cv.imread("../images/3.jpg")
+img2 = cv.imread("../images/4.png")
 
 # added
 # added = cv.add(img1, img2)
 # added = img1 + img2
-added = cv.addWeighted(img1, 0.6, img2, 0.4, 0)
+added = cv.addWeighted(img1, 0.2, img2, 0.8, 0)
 
 cv.imshow("added", added)
 cv.waitKey(0)
